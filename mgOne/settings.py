052ac14857f5f680d250fadfe647231f7034013b
vishnu_96 <vishnu96.ray@gmail.com>
2022-10-09 00:18:58 +0530

    changes

--- mgOne/settings.py
+++ mgOne/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)*7m&+uxub&ucdi*nv$0l-g1g&+o$-)q!o*1rbuves3rx1#)7j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
